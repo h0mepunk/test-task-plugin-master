9802c6d1af6ec833c4c15b91c55e374b64d9c9cd
homepunk <mkasatkina@fun.co>
2023-07-08 15:31:48 +0300

    added getKotlinPluginVersion() to Task1

--- src/main/kotlin/org/jetbrains/TestTaskAction1.kt
+++ src/main/kotlin/org/jetbrains/TestTaskAction1.kt
@@ -1,8 +1,10 @@
 package org.jetbrains
 
+import com.intellij.ide.plugins.PluginManager
 import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 
@@ -13,9 +15,20 @@ class TestTaskAction1 : AnAction() {
         // Using the event, evaluate the context, and enable or disable the action.
     }
 
+    private fun getKotlinPluginVersion(): String? {
+        val kotlinPluginId = PluginId.findId("org.jetbrains.kotlin")
+        val kotlinPluginInstalled: Boolean = PluginManager.isPluginInstalled(kotlinPluginId)
+
+        if (kotlinPluginInstalled) {
+            return PluginManager.getPlugin(kotlinPluginId)?.version
+        } else {
+            return "Plugin is not installed :c"
+        }
+    }
+
     override fun actionPerformed(e: AnActionEvent) {
         //Using the event, implement an action. For example, create and show a dialog.
         val currentProject: Project = e.project!!
-        Messages.showMessageDialog(currentProject, "Here should be output of task 1", "Task 1", Messages.getInformationIcon())
+        Messages.showMessageDialog(currentProject, getKotlinPluginVersion(), "Task 1", Messages.getInformationIcon())
     }
 }
